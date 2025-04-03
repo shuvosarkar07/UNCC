910fe9292e4dab6a7f7dfefca3328cae1b3097c6
shuvo sarkar <contact.shuvosarkar@gmail.com>
2025-04-03 16:25:39 +0530

    improve using eventemitter

--- 03-File-system/Project/app.js
+++ 03-File-system/Project/app.js
@@ -1,26 +1,35 @@
 const fs = require("fs/promises");
+const { EventEmitter } = require('events');
 
 (async () => {
   const commandFileHandler = await fs.open("./command.txt");
-  const watcher = fs.watch("./command.txt");
+  const eventEmitter = new EventEmitter();
 
+
+  eventEmitter.on("fileChanged", async () => {
+
+    const stat = await commandFileHandler.stat();
+    const buffer = Buffer.alloc(stat.size);
+    const size = stat.size;
+    const offset = 0;
+    const position = 0;
+
+    const content = await commandFileHandler.read(
+      buffer,
+      offset,
+      size,
+      position
+    );
+    console.log(content.buffer.toString('utf-8'));
+  });
+
+
+  const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      console.log("file changed");
-
-      const stat = await commandFileHandler.stat();
-      const buffer = Buffer.alloc(stat.size);
-      const size = stat.size;
-      const offset = 0;
-      const encoding = "utf-8";
-
-      const content = await commandFileHandler.read(
-        buffer,
-        offset,
-        size,
-        encoding
-      );
-      console.log(content.toString());
+      eventEmitter.emit("fileChanged");
     }
   }
+  
+  await commandFileHandler.close();
 })();
