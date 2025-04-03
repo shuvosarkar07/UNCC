93b6aba2e4ce29e0de87e8d13153b161d7780786
shuvo sarkar <contact.shuvosarkar@gmail.com>
2025-04-03 15:56:12 +0530

    Enhance file system watcher to read and log changes from command.txt

--- 03-File-system/Project/app.js
+++ 03-File-system/Project/app.js
@@ -1,9 +1,26 @@
 const fs = require("fs/promises");
 
 (async () => {
-  const watcher = fs.watch("./");
+  const commandFileHandler = await fs.open("./command.txt");
+  const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
-    console.log(event);
+    if (event.eventType === "change") {
+      console.log("file changed");
+
+      const stat = await commandFileHandler.stat();
+      const buffer = Buffer.alloc(stat.size);
+      const size = stat.size;
+      const offset = 0;
+      const encoding = "utf-8";
+
+      const content = await commandFileHandler.read(
+        buffer,
+        offset,
+        size,
+        encoding
+      );
+      console.log(content.toString());
+    }
   }
 })();
